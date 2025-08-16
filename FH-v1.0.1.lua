82e1867a92e894f5b05a6f852a861d200dd84d1d
FroxyCreates1 <do1331679@gmail.com>
2025-08-15 23:09:53 -0400

    Update FH-v1.0.1.lua

--- FH-v1.0.1.lua
+++ FH-v1.0.1.lua
@@ -319,6 +319,40 @@ createButton("[TEST] GANGNAM STYLE", function()
     end)
 end)
 
+createButton("[UNIVERSAL] C00LKIDD CLOTHING", function()
+    local Players = game:GetService("Players")
+    local player = Players.LocalPlayer
+    local char = player.Character or player.CharacterAdded:Wait()
+
+    -- CHANGE THESE TO YOUR SHIRT + PANTS IDs IF U WANT
+    local shirtId = "rbxassetid://16012481458" 
+    local pantsId = "rbxassetid://107896578601954" 
+    -- ==============================================
+
+    -- Turn character red
+    for _, part in ipairs(char:GetDescendants()) do
+        if part:IsA("BasePart") then
+            part.Color = Color3.fromRGB(255, 0, 0)
+        end
+    end
+
+    -- Remove old clothing if it exists
+    if char:FindFirstChildOfClass("Shirt") then
+        char:FindFirstChildOfClass("Shirt"):Destroy()
+    end
+    if char:FindFirstChildOfClass("Pants") then
+        char:FindFirstChildOfClass("Pants"):Destroy()
+    end
+
+    -- Add new clothing
+    local shirt = Instance.new("Shirt")
+    shirt.ShirtTemplate = shirtId
+    shirt.Parent = char
+
+    local pants = Instance.new("Pants")
+    pants.PantsTemplate = pantsId
+    pants.Parent = char
+end)
 	
 
 local spawnPartsActive = false
